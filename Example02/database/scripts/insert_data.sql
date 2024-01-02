ed945eee62c8ba4befb419f1841a47dbf64ccd38
heondong9265 <heondong0723@gmail.com>
2024-01-02 17:53:33 +0900

    feat: 문제 준비중

---
+++ Example02/database/scripts/insert_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO visits (visitor_name) VALUES ('김동헌');
+INSERT INTO visits (visitor_name) VALUES ('김민기');
+INSERT INTO visits (visitor_name) VALUES ('김윤기');
+INSERT INTO visits (visitor_name) VALUES ('설한나');
